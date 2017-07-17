c5ab97b5d33283255dffc262ebbb9bd64e72a099
Ben Abelshausen <ben@osmsharp.com>
2017-07-17 14:16:30 +0200

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using NetTopologySuite.Features;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTS_BufferingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTopologySuite.IO.GeoJsonSerializer serialize = new NetTopologySuite.IO.GeoJsonSerializer();
+            var inputFile = File.OpenText("map.geojson");
+            var features = serialize.Deserialize<FeatureCollection>(new JsonTextReader(inputFile));
+
+            var bufferedFeatures = new FeatureCollection();
+            foreach(var feature in features.Features)
+            {
+                var op = new NetTopologySuite.Operation.Buffer.BufferOp(feature.Geometry);
+                var geometry = op.GetResultGeometry(0.02);
+
+                var bufferedFeature = new Feature(geometry, new AttributesTable());
+
+                bufferedFeatures.Add(bufferedFeature);
+            }
+      
+            using (var outputFile = new StreamWriter(File.OpenWrite("buffered.geojson")))
+            {
+                serialize.Serialize(outputFile, bufferedFeatures);
+            }
+        }
+    }
+}
